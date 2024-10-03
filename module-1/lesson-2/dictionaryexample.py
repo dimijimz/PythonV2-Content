056cadb40d3b418f450c0f353444c09b0c278fc2
dimijimz <dimijimz96@gmail.com>
2024-10-02 22:11:43 -0400

    complete assignments and add more lessons

---
+++ module-1/lesson-2/dictionaryexample.py
@@ -0,0 +1,17 @@
+##my_dict = {'name': 'Dimitri', 'age': 28}
+
+##for value in my_dict.values():
+  ##  print(my_dict)
+
+
+school = {'class_one' :
+[0{'name': 'Dimitri', 'age': 28},
+{'name': 'Timmy', 'age': 22},
+{'name': 'Sally', 'age': 30},
+{'name': 'Sue', 'age': 25}],
+         'class_two' :
+[{'name': 'Joan', 'age': 21},
+{'name': 'Tony', 'age': 24},
+{'name': 'Tina', 'age': 31}]
+}
+total_age = 0
---
+++ module-1/lesson-2/lists.py
@@ -0,0 +1,17 @@
+# Lists
+#my_list = ["tire", "wheel", "seat", "brake"]
+
+#my_list.append(4)
+
+#print(my_list)
+
+
+###next lesson
+##nested_list=[[1,2,3],[4,5,6],[7,8,9]     ]
+
+##print(nested_list[1][2])
+
+##next lesson
+my_list=[5,2,6,7,55,11,97,54,65]
+## Min print - print('min:', min(my_list))
+## Max print - print('max:', max(my_list))
