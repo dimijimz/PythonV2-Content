c487e5e78c988708c65ed918347ccf0857b209b4
Dimitri Jimenez <dimijimz96@gmail.com>
2025-01-06 20:35:30 -0500

    Unstaged changes

--- Module - 1/Lesson-6-Conditional-Statements-Loops/conditions and controls trial.py
+++
@@ -1,9 +0,0 @@
-nm = int(input("Enter a number: "))
-if nm >= 0:
-    if nm == 0:
-        print("The number is zero.")
-    else:
-        print("The number is positive.")
-else:
-    print("The number is negative.")
-
